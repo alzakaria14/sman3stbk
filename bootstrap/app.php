f618cd7c353d75b2f951f6fc8bb445372e9c6501
Al Zakaria <alzakaria@ruangkarya.id>
2026-06-21 19:23:18 +0800

    commit before

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$basePath = dirname(__DIR__);
+
+$app = Application::configure(basePath: $basePath)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -22,3 +24,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+$publicHtmlPath = $basePath.'/public_html';
+
+if (is_dir($publicHtmlPath)) {
+    $app->usePublicPath($publicHtmlPath);
+}
+
+return $app;
